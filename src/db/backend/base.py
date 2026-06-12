4feef48e54e11864d762cb16615da52e53daa6f5
KVKALASHNIKOV <kirill840251@gmail.com>
2026-06-12 23:07:10 +0300

    Update base.py

--- src/db/backend/base.py
+++ src/db/backend/base.py
@@ -1,1 +1,36 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Tuple
 
+
+class BaseDB(ABC):
+
+    @abstractmethod
+    def add_record(self, data: Dict[str, Any]) -> int:
+        pass
+
+    @abstractmethod
+    def get_all_records(self) -> List[Tuple[int, Dict[str, Any]]]:
+        pass
+
+    @abstractmethod
+    def get_record(self, record_id: int) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def filter_records(
+        self,
+        filters: Dict[str, Any]
+    ) -> List[Tuple[int, Dict[str, Any]]]:
+        pass
+
+    @abstractmethod
+    def update_record(
+        self,
+        record_id: int,
+        data: Dict[str, Any]
+    ) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def delete_record(self, record_id: int) -> Dict[str, Any]:
+        pass
